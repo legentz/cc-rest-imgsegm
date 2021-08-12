b9fe6292a34fab730da38ffe71f27d677e7e23c1
Leandro Gentili <leandrogentili@gmail.com>
2021-08-12 19:15:36 +0200

    dropzone sample

--- webapp/main.js
+++ webapp/main.js
@@ -1,8 +1,25 @@
 (function () {
 	var pageReady = "DOMContentLoaded";
+	var imageDropzone = document.getElementById("image-to-process");
+	var sendImageBtn = document.getElementById("send-multipart-request");
 
-	document.addEventListener(pageReady, function () {
+	/**
+		Events
+	*/
+
+	// pageReady
+	document.addEventListener(pageReady, function (e) {
 		console.log(pageReady);
 	});
+
+	/**
+		Actions
+	*/
+
+	// Send image to process
+	sendImageBtn.onclick = function (e) {	
+		console.log("Clicked on sendImageBtn");
+		// console.log(payload);
+	};
 
 })();
