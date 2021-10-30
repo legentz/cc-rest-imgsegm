ec4e261a87bf894600266b71cb29bb42764fa360
Leandro Gentili <leandrogentili@gmail.com>
2021-10-30 15:15:08 +0200

    removing unused methods from utils.py

--- server/model/utils.py
+++ server/model/utils.py
@@ -89,87 +89,6 @@ within the Notebook of the project.
 '''
 class Images(object):
 
-	'''
-	This method plots a ndarray of images along n_rows/n_cols (as sub-plot)
-	'''
-	@staticmethod
-	def plot_arr_to_imgs(arr, n_max=-1, cols=1, cmap=None, figsize=(15, 15)):
-		n_imgs = len(arr)			# how many images
-		rows = ceil(n_imgs / cols)	# how many rows
-
-		# set figure size
-		fig = plt.figure(figsize=figsize)
-
-		for i in range(0, cols * rows):
-
-			# load n_max images
-			if i >= n_max: break
-
-			# set a sub-plot for inline visualization
-			fig.add_subplot(rows, cols, i + 1)
-
-			# get the image depending on the input shape
-			# ex. (10, 512, 512, 1) -> 10 512x512 images on 1 channel
-			if len(arr.shape) == 4:
-				img = arr[i, :, :, 0]
-
-			elif len(arr.shape) == 3:
-				img = arr[:, :, i]
-
-			# we can assume it's width and height
-			elif len(arr.shape) == 2:
-				img = arr
-
-			else: raise('Bad shaped array provided!')
-
-			# add image to grid
-			plt.imshow(img, cmap=cmap)
-
-		# show
-		plt.show()
-
-	'''
-	This method is almost a duplicate of the Iterator a ndarray of images along n_rows/n_cols (as sub-plot)
-	'''
-	@staticmethod
-	def plot_imgs_from_folder(path, n_max=-1, cols=1, cmap=None, figsize=(15, 15)):
-		assert os.path.exists(path)
-
-		# we need to sort to be sure the order is maintained
-		# our files are always 0.png, 1.png, ...
-		# TODO: handle labels instead of sorting
-		imgs = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-		imgs.sort()
-		n_imgs = len(imgs) 
-		rows = ceil(n_imgs / cols) 
-
-		# set figure size
-		fig = plt.figure(figsize=figsize)
-
-		# loop over images number
-		for i in range(0, n_imgs):
-
-			# load n_max images
-			if i >= n_max: break
-
-			fig.add_subplot(rows, cols, i + 1)
-
-			# TODO: make this a standalone function under Images utils
-			try:
-				img = Image.open(os.path.join(path, imgs[i]))
-			except:
-				print('WARNING:', exc_info()[0])
-				continue
-
-			# img to ndarray
-			img = np.asarray(img)
-
-			# add image to grid
-			plt.imshow(img, cmap=cmap)
-
-		# show
-		plt.show()
-
 	@staticmethod
 	def save_as_imgs(output_dir, preds, f_name2path, use_format='.png'):
 		assert output_dir is not None
@@ -198,7 +117,3 @@ class Images(object):
 			f_name2path[f_name]['output'] = im_path
 
 		return f_name2path
-
-
-
-
