9fb13056b90f434e67e75d78e37f1edafcf43741
christian sordi <sordi.1748457@studenti.uniroma1.it>
2022-01-23 17:00:15 +0100

    Added Aws init script and Docker deamon config + join cluster

---
+++ aws/aws-ec2-init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+sudo apt upgrade -y
+sudo apt-get update
